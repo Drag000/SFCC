2132879c2127c57c205ba542b6d249d6ddf3d2cb
Drag000 <113060670+Drag000@users.noreply.github.com>
2025-02-27 11:42:39 +0200

    Discount Callout updates

--- cartridges/app_training/cartridge/controllers/Tile.js
+++ cartridges/app_training/cartridge/controllers/Tile.js
@@ -1,29 +1,23 @@
 'use strict';
- 
+
 var server = require('server');
 var baseProductHelpers = require('*/cartridge/scripts/helpers/productHelpers');
- 
+
 server.extend(module.superModule);
 
 server.append('Show', function (req, res, next) {
     var discountPercentage = null;
-    
-    
-    discountPercentage = baseProductHelpers.calculatePercentageOff(100, 70);
-        
-    res.setViewData({
-        discountPercentage: discountPercentage
-    });
-    
+
+    var product = res.getViewData().product;
+
+    if (product.price.sales && product.price.list) {
+        discountPercentage = baseProductHelpers.calculatePercentageOff(product.price.list.value, product.price.sales.value);
+    }
+
+    res.setViewData({ discountPercentage: discountPercentage });
+
     return next();
 })
 
-module.exports = server.exports(); 
+module.exports = server.exports();
 
-
-
-// var discountPercentage = null;
-    
-// if (product.price.sales) {
-//     print(product.price.sales)  
-// }
