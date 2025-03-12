b147413874a65855cddc96b00f7276c25a1ecc9c
Drag000 <113060670+Drag000@users.noreply.github.com>
2025-03-12 15:09:05 +0200

    Updating Cart.js

--- cartridges/app_training/cartridge/controllers/Cart.js
+++ cartridges/app_training/cartridge/controllers/Cart.js
@@ -3,8 +3,6 @@
 var server = require('server');
 server.extend(module.superModule);
 
-
-
 server.append('Show', function (req, res, next) {
     var BasketMgr = require('dw/order/BasketMgr');
     var Site = require('dw/system/Site');
@@ -22,49 +20,5 @@ server.append('Show', function (req, res, next) {
 
     return next();
 });
-
-
-
-// server.append('RemoveProductLineItem', function (req, res, next) {
-//     var BasketMgr = require('dw/order/BasketMgr');
-//     var Site = require('dw/system/Site');
-//     var currentBasket = BasketMgr.getCurrentBasket();
-//     var warninigMessage;
-//     var Transaction = require('dw/system/Transaction');
-//     var basketCalculationHelpers = require('*/cartridge/scripts/helpers/basketCalculationHelpers');
-
-
-//     // var cartTotal = currentBasket.totalGrossPrice.value;
-//     // var cartTotalThreshold = Site.getCurrent().getCustomPreferenceValue('cartTotalThreshold');
-
-//     // var viewData = res.getViewData();
-
-//     if (currentBasket) {
-//         // Recalculate the cart total after removing a product
-//         Transaction.wrap(function () {
-//             // Call the basket calculation logic to update totals
-//             basketCalculationHelpers.calculateTotals(currentBasket);
-
-//             var cartTotal = currentBasket.totalGrossPrice.value;
-//             var cartTotalThreshold = Site.getCurrent().getCustomPreferenceValue('cartTotalThreshold');
-
-//             // Compare the recalculated cart total with the threshold
-//             if (cartTotal > cartTotalThreshold) {
-//                 warninigMessage = 'Your cart total exceeds $' + cartTotalThreshold + '!';
-//             } else {
-//                 warninigMessage = 'ASD';
-//             }
-//         });
-//     }
-
-
-//     // if (cartTotal < cartTotalThreshold) {
-//     //     warninigMessage = 'asd';
-//     // }
-
-//     res.setViewData({ warninigMessage: warninigMessage });
-
-//     return next();
-// });
 
 module.exports = server.exports();
