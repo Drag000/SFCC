85551e3ce8e644fe234df9ed342914761dde86f7
Drag000 <113060670+Drag000@users.noreply.github.com>
2025-03-20 11:31:02 +0200

    Adding Docstrings

--- cartridges/app_training/cartridge/controllers/Unsplash.js
+++ cartridges/app_training/cartridge/controllers/Unsplash.js
@@ -3,6 +3,18 @@
 var server = require('server');
 var unsplashService = require('*/cartridge/scripts/services/UnsplashService');
 
+
+/**
+* Handles the request to retrieve images from the Unsplash service and renders the response.
+* 
+* This function makes a call to the Unsplash service to fetch images. If the service call is 
+* successful, it renders the images using the 'unsplash/images' template. In case of failure, 
+* it sets the response status code to 500 and returns a JSON object with an error message.
+* 
+* @param {Object} req - The request object containing the HTTP request information.
+* @param {Object} res - The response object used to send the HTTP response.
+* @param {Function} next - The next middleware function in the stack.
+*/
 server.get('Show', function (req, res, next) {
     var result = unsplashService.call();
 
