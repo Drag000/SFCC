7c9d5d4d36f6c534f028ce3df0abe4832502462c
Drag000 <113060670+Drag000@users.noreply.github.com>
2025-03-24 16:33:43 +0200

    Exercise Banner Component

---
+++ cartridges/app_training/cartridge/experience/components/assets/custombanner.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var Template = require('dw/util/Template');
+var HashMap = require('dw/util/HashMap');
+var URLUtils = require('dw/web/URLUtils');
+var ImageTransformation = require('~/cartridge/experience/utilities/ImageTransformation.js');
+
+/**
+ * Render logic for the assets.custombanner.
+ */
+module.exports.render = function (context) {
+    var model = new HashMap();
+    var content = context.content;
+    var category = content.category;
+
+    if (content.image) {
+        model.image = {
+            src: {
+                mobile: ImageTransformation.url(content.image, { device: 'mobile' }),
+                desktop: ImageTransformation.url(content.image, { device: 'desktop' })
+            },
+            alt: content.image.file.getAlt(),
+            focalPointX: content.image.focalPoint.x * 100 + '%',
+            focalPointY: content.image.focalPoint.y * 100 + '%'
+        };
+    }
+
+    if (category) {
+        model.url = URLUtils.url('Search-Show', 'cgid', category.ID);
+    } else {
+        model.url = URLUtils.url('Home-Show');
+    }
+
+    return new Template('experience/components/assets/custombanner').render(model).text;
+};
