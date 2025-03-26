71cae4e027c6cd18e68535c1f900bfad1d19cbec
Drag000 <113060670+Drag000@users.noreply.github.com>
2025-03-26 23:07:40 +0200

    COde updates

--- cartridges/module_pagedesigner/cartridge/experience/components/assets/wayfinderAsset.js
+++ cartridges/module_pagedesigner/cartridge/experience/components/assets/wayfinderAsset.js
@@ -4,6 +4,7 @@ var Template = require('dw/util/Template');
 var HashMap = require('dw/util/HashMap');
 var URLUtils = require('dw/web/URLUtils');
 var ImageTransformation = require('~/cartridge/experience/utilities/ImageTransformation.js');
+var ImageTransformationHelper = require('~/cartridge/scripts/helpers/ImageTransformationHelper.js');
 
 /**
  * Render logic for the assets.wayfinderAsset component.
@@ -13,24 +14,8 @@ module.exports.render = function (context) {
     var content = context.content;
     var category = content.category;
 
-    if (content.image) {
-        model.image = {
-            src: {
-                mobile: ImageTransformation.url(content.image, { device: 'mobile', width: 300, height: 200, crop: 'fill' }),
-                desktop: ImageTransformation.url(content.image, { device: 'desktop', width: 300, height: 200, crop: 'fill' })
-            },
-            alt: content.image.file.getAlt(),
-            focalPointX: content.image.focalPoint.x * 100 + '%',
-            focalPointY: content.image.focalPoint.y * 100 + '%'
-        };
-    }
-
-    if (category) {
-        model.url = URLUtils.url('Search-Show', 'cgid', category.ID);
-    } else {
-        model.url = URLUtils.url('Home-Show');
-    }
-    
+    model.image = ImageTransformationHelper.getScaledImage(content.image);
+    model.url = URLUtils.url('Search-Show', 'cgid', category.ID);
     model.title = content.title;
 
     return new Template('experience/components/assets/wayfinderAsset').render(model).text;
