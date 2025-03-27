97290cec777fb1eb1f98c5ebbe30b5f5f92ede99
Drag000 <113060670+Drag000@users.noreply.github.com>
2025-03-27 16:43:12 +0200

    Adding Docstring

--- cartridges/app_training/cartridge/controllers/Search.js
+++ cartridges/app_training/cartridge/controllers/Search.js
@@ -6,6 +6,16 @@ var CatalogMgr = require('dw/catalog/CatalogMgr');
  
 server.extend(module.superModule);
 
+/**
+* Appends the 'Show' route to the server, which handles the display of a category page.
+* If a Page Designer page is associated with the category and is visible, it renders the page.
+* Otherwise, it proceeds to the next middleware in the chain.
+*
+* @param {Object} req - The server request object, containing query parameters and other request data.
+* @param {Object} res - The server response object, used to send responses to the client.
+* @param {Function} next - The next middleware function in the server's request-response cycle.
+* @returns {void} - This function does not return a value. It either renders a page or calls the next middleware.
+*/
 server.append('Show', function (req, res, next) {
     var catId = req.querystring.cgid;
     var category = CatalogMgr.getCategory(catId);
--- cartridges/module_pagedesigner/cartridge/experience/components/assets/custombanner.js
+++ cartridges/module_pagedesigner/cartridge/experience/components/assets/custombanner.js
@@ -6,9 +6,20 @@ var URLUtils = require('dw/web/URLUtils');
 var ImageTransformation = require('~/cartridge/experience/utilities/ImageTransformation.js');
 var ImageTransformationHelper = require('~/cartridge/scripts/helpers/ImageTransformationHelper.js');
 
+
 /**
- * Render logic for the assets.custombanner.
- */
+* Renders a custom banner component for the storefront.
+*
+* This function generates a model for the custom banner component using the provided context.
+* It processes the content to include an image and a URL based on the category information.
+* The final output is rendered using a specific template.
+*
+* @param {Object} context - The context object containing content and other relevant data.
+* @param {Object} context.content - The content object containing details for rendering the banner.
+* @param {dw.catalog.Category} [context.content.category] - The category associated with the banner, if any.
+* @param {dw.content.MediaFile} [context.content.image] - The image to be displayed in the banner, if provided.
+* @returns {string} The rendered HTML text of the custom banner component.
+*/
 module.exports.render = function (context) {
     var model = new HashMap();
     var content = context.content;
