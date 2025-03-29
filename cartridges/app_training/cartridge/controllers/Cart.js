5f7c9785899d0396912d81febcd1e030a36f4a74
Drag000 <113060670+Drag000@users.noreply.github.com>
2025-03-29 15:55:36 +0200

    Docsting + code optimization

--- cartridges/app_training/cartridge/controllers/Cart.js
+++ cartridges/app_training/cartridge/controllers/Cart.js
@@ -4,19 +4,39 @@ var server = require('server');
 
 server.extend(module.superModule);
 
+
+/**
+* Appends a product to the current basket and performs necessary operations such as
+* calculating totals and sending email notifications if applicable.
+*
+* @function
+* @param {Object} req - The request object containing form data and customer information.
+* @param {Object} res - The response object.
+* @param {Function} next - The next middleware function in the chain.
+*
+* @description
+* This function handles the addition of a product to the shopping cart. It retrieves the
+* current or new basket, adds the specified product with the given quantity, ensures all
+* shipments have methods, and calculates the basket totals. If the customer is logged in,
+* it sends an email notification about the added product, including product details and
+* image URL.
+*
+*/
 server.append('AddProduct', function (req, res, next) {
     var BasketMgr = require('dw/order/BasketMgr');
     var Transaction = require('dw/system/Transaction');
+    var ProductMgr = require('dw/catalog/ProductMgr');
+    var URLUtils = require('dw/web/URLUtils');
     var cartHelper = require('*/cartridge/scripts/cart/cartHelpers');
     var basketCalculationHelpers = require('*/cartridge/scripts/helpers/basketCalculationHelpers');
-    var ProductMgr = require('dw/catalog/ProductMgr');
-    var URLUtils = require('dw/web/URLUtils')
     var cartEmailHelpers = require('*/cartridge/scripts/helpers/cartEmailHelpers');
+    
     var quantity = parseInt(req.form.quantity, 10);
     var currentBasket = BasketMgr.getCurrentOrNewBasket();
     var customer = req.currentCustomer;
     var productId = req.form.pid;
-
+    
+    
     if (currentBasket) {
         Transaction.wrap(function () {
             var result = cartHelper.addProductToCart(currentBasket, productId, quantity);
@@ -24,22 +44,21 @@ server.append('AddProduct', function (req, res, next) {
             if (!result.error) {
                 cartHelper.ensureAllShipmentsHaveMethods(currentBasket);
                 basketCalculationHelpers.calculateTotals(currentBasket);
-
-                // Send email notification if customer is logged in
-                if (customer.profile) {
-                    var product = ProductMgr.getProduct(productId);
-                    var protocol = req.https ? 'https://' : 'http://';
-                    var host = req.host;
-                    var baseURL = protocol + host;
-                    var imageURL = product.getImage('large', 0).getURL();
-                    var productImageURL = baseURL + imageURL;
-
-                    var productURL = URLUtils.abs('Product-Show', 'pid', product.ID).toString();
-                    
-                    cartEmailHelpers.sendCartEmailNotification(customer, product, quantity, productURL, productImageURL);
-                }
             }
         });
+        
+        // Send email notification if customer is logged in
+        if (customer.profile) {
+            var product = ProductMgr.getProduct(productId);
+            var protocol = req.https ? 'https://' : 'http://';
+            var host = req.host;
+            var baseURL = protocol + host;
+            var imageURL = product.getImage('large', 0).getURL();
+            var productImageURL = baseURL + imageURL
+            var productURL = URLUtils.abs('Product-Show', 'pid', product.ID).toString();
+            
+            cartEmailHelpers.sendCartEmailNotification(customer, product, quantity, productURL, productImageURL);
+        }
     }
 
     next();
