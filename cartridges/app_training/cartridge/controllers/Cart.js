9bdc700ebdd98fa2c53f9b5980527a355a9397eb
Drag000 <113060670+Drag000@users.noreply.github.com>
2025-03-29 12:20:34 +0200

    Add Docstring

--- cartridges/app_training/cartridge/controllers/Cart.js
+++ cartridges/app_training/cartridge/controllers/Cart.js
@@ -6,6 +6,16 @@ var Site = require('dw/system/Site');
 
 server.extend(module.superModule);
 
+/**
+* Appends a 'Show' route to the server, which checks the current basket's total
+* against a predefined threshold and sets a warning message if the total exceeds
+* this threshold. The warning message and threshold are then passed to the view data.
+*
+* @param {Object} req - The request object provided by the server
+* @param {Object} res - The response object provided by the server
+* @param {Function} next - The next middleware function in the stack
+* @returns {void} - The function does not return a value, but passes control to the next middleware
+*/
 server.append('Show', function (req, res, next) {
     var currentBasket = BasketMgr.getCurrentBasket();
     var warninigMessage;
