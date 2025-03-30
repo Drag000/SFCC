767422620217daf9532b540b7124b28147fd01e6
Drag000 <113060670+Drag000@users.noreply.github.com>
2025-03-30 19:45:43 +0300

    Add metadata + code optimization

--- cartridges/app_training/cartridge/controllers/Unsplash.js
+++ cartridges/app_training/cartridge/controllers/Unsplash.js
@@ -3,20 +3,24 @@
 var server = require('server');
 var unsplashService = require('*/cartridge/scripts/services/UnsplashService');
 
-
 /**
-* Handles the request to retrieve images from the Unsplash service and renders the response.
-* 
-* This function makes a call to the Unsplash service to fetch images. If the service call is 
-* successful, it renders the images using the 'unsplash/images' template. In case of failure, 
-* it sets the response status code to 500 and returns a JSON object with an error message.
-* 
-* @param {Object} req - The request object containing the HTTP request information.
-* @param {Object} res - The response object used to send the HTTP response.
+* Handles the 'Show' route to fetch and display images from the Unsplash service.
+*
+* This function makes a call to the Unsplash service to retrieve a list of photos.
+* If the service call is successful, it renders the images using the 'unsplash/images' template.
+* If the service call fails, it responds with a 500 status code and an error message.
+*
+* @param {Object} req - The server request object.
+* @param {Object} res - The server response object.
 * @param {Function} next - The next middleware function in the stack.
 */
 server.get('Show', function (req, res, next) {
-    var result = unsplashService.call();
+    var params = {
+        endpoint: 'photos',
+        page: 1,
+    };
+    
+    var result = unsplashService.call(params);
 
     if (result.ok) {
         res.render('unsplash/images', { images: result.object });
