f2fcf1fe04cd49f021acdfc446e34da368cdffa5
Drag000 <113060670+Drag000@users.noreply.github.com>
2025-03-31 13:03:11 +0300

    Metadata folder update + Docstring

--- cartridges/app_training/cartridge/controllers/Search.js
+++ cartridges/app_training/cartridge/controllers/Search.js
@@ -6,6 +6,18 @@ var CatalogMgr = require('dw/catalog/CatalogMgr');
  
 server.extend(module.superModule);
 
+/**
+* Appends the 'Show' route to the server, rendering a page using Page Designer if applicable.
+*
+* This function retrieves a category based on the category ID from the request query string.
+* If the category has a Page Designer page ID and the corresponding page is visible, it renders
+* the page using the Page Manager. Otherwise, it proceeds to the next middleware in the chain.
+*
+* @param {Object} req - The server request object containing the query string parameters.
+* @param {Object} res - The server response object used to send responses to the client.
+* @param {Function} next - The next middleware function in the server's request-response cycle.
+* @returns {undefined} - The function does not return a value but may render a page or call the next middleware.
+*/
 server.append('Show', function (req, res, next) {
     var catId = req.querystring.cgid;
     var category = CatalogMgr.getCategory(catId);
