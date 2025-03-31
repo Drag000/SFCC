768b578e1731a308f5542264fd48ede28b46ca6d
Drag000 <113060670+Drag000@users.noreply.github.com>
2025-03-31 10:28:09 +0300

    Add Docstring

--- cartridges/app_training/cartridge/controllers/Cart.js
+++ cartridges/app_training/cartridge/controllers/Cart.js
@@ -7,31 +7,37 @@ var Site = require('dw/system/Site');
 server.extend(module.superModule);
 
 /**
-* Appends a 'Show' route to the server, which checks the current basket's total
-* against a predefined threshold and sets a warning message if the total exceeds
-* this threshold. The warning message and threshold are then passed to the view data.
+* Appends the 'Show' route to the server, which checks the current basket's total gross price
+* against a predefined threshold and sets a warning message if the threshold is exceeded.
 *
-* @param {Object} req - The request object provided by the server
-* @param {Object} res - The response object provided by the server
-* @param {Function} next - The next middleware function in the stack
-* @returns {void} - The function does not return a value, but passes control to the next middleware
+* @param {Object} req - The request object, representing the HTTP request.
+* @param {Object} res - The response object, used to send data back to the client.
+* @param {Function} next - The next middleware function in the stack.
+* @returns {Function} - Calls the next middleware function in the stack.
+*
+* @description This function retrieves the current basket using the BasketMgr, calculates the total
+* gross price of the cart, and compares it to a custom preference value 'cartTotalThreshold' from
+* the Site object. If the cart total exceeds the threshold, a warning message is generated and
+* added to the view data. The function then proceeds to the next middleware in the chain.
 */
 server.append('Show', function (req, res, next) {
     var currentBasket = BasketMgr.getCurrentBasket();
     var warninigMessage;
-
+    
     var cartTotal = currentBasket.totalGrossPrice.value;
-    var cartTotalThreshold = Site.getCurrent().getCustomPreferenceValue('cartTotalThreshold');
-
-    if (cartTotal > cartTotalThreshold) {
-        warninigMessage = 'Your cart total exceeds $' + cartTotalThreshold + '!';
+    if (cartTotal) {
+        
+        var cartTotalThreshold = Site.getCurrent().getCustomPreferenceValue('cartTotalThreshold');
+    
+        if (cartTotal > cartTotalThreshold) {
+            warninigMessage = 'Your cart total exceeds $' + cartTotalThreshold + '!';
+        }
+    
+        res.setViewData({ 
+            warninigMessage: warninigMessage,
+            cartTotalThreshold: cartTotalThreshold
+        });
     }
-
-    res.setViewData({ 
-        warninigMessage: warninigMessage,
-        cartTotalThreshold: cartTotalThreshold
-    });
-
     return next();
 });
 
